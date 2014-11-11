0de96997501e576e41aaaf3b53879f83d12afdaf
jschintz <james.schintz@gmail.com>
2014-11-11 12:36:06 -0500

    Update Export.js

--- Export.js
+++ Export.js
@@ -3,6 +3,27 @@
 //  Note: this line does not appear in the tutorial videos but should still be used
 //    and is in all of the included js files. 
 
+var viz, workbook;
+
+window.onload= function() {
+	var vizDiv = document.getElementById('viz');
+	var vizURL = "http://243ewmp01.blackmesh.com/t/FSDKenya/views/FinancialAccessDemo/ExploreDemographics";
+	var options = {
+		width: '600px',
+		height: '540px',
+		hideToolbar: true,
+		hideTabs: true,
+		onFirstInteractive: function () {
+			workbook = viz.getWorkbook();
+			document.getElementById('sheetName').innerHTML = viz.getWorkbook().getActiveSheet().getName();
+		}
+	};
+	viz = new tableauSoftware.Viz(vizDiv, vizURL, options);
+	viz.addEventListener('tabswitch', function(event) {
+		document.getElementById('sheetName').innerHTML = event.getNewSheetName();
+	});
+};
+
 function exportPDF() {
 	viz.showExportPDFDialog();
 }
